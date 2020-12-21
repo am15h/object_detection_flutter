8186662c40d52f6a9198b7742dd7d02dcfbfa51d
Bažináč <kuceravej@gmail.com>
2020-12-21 21:53:33 +0100

    Makes conversion work also on the iOS (#4)
    * Makes conversion work also on the iOS
    
    * Makes conversion work also on the iOS,
    + correcting format for fromBytes.
    
    * Revert "Makes conversion work also on the iOS, + correcting format for fromBytes."
    
    This reverts commit 3f5a0536
    
    * Revert "Makes conversion work also on the iOS, + correcting format for fromBytes."
    
    This reverts commit 3f5a0536
    
    * Revert "Makes conversion work also on the iOS, + correcting format for fromBytes."
    
    This reverts commit 3f5a0536

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -7,6 +7,25 @@ import 'package:path_provider/path_provider.dart';
 /// ImageUtils
 class ImageUtils {
   /// Converts a [CameraImage] in YUV420 format to [imageLib.Image] in RGB format
+  static imageLib.Image convertCameraImage(CameraImage cameraImage) {
+    if (cameraImage.format.group == ImageFormatGroup.yuv420) {
+      return convertYUV420ToImage(cameraImage);
+    } else if (cameraImage.format.group == ImageFormatGroup.bgra8888) {
+      return convertBGRA8888ToImage(cameraImage);
+    } else {
+      return null;
+    }
+  }
+
+  /// Converts a [CameraImage] in BGRA888 format to [imageLib.Image] in RGB format
+  static imageLib.Image convertBGRA8888ToImage(CameraImage cameraImage) {
+    imageLib.Image img = imageLib.Image.fromBytes(cameraImage.planes[0].width,
+        cameraImage.planes[0].height, cameraImage.planes[0].bytes,
+        format: imageLib.Format.bgra);
+    return img;
+  }
+
+  /// Converts a [CameraImage] in YUV420 format to [imageLib.Image] in RGB format
   static imageLib.Image convertYUV420ToImage(CameraImage cameraImage) {
     final int width = cameraImage.width;
     final int height = cameraImage.height;
